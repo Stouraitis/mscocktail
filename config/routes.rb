109ee554512abd28acaeef0fd84d920dcc796ea7
Yiannis Stouraiti <iostouraitis@gmail.com>
2016-08-11 12:50:01 +0200

    models, validations, association are ok _2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+
+  resources :cocktails do
+    resources :dose do
+      resources :ingredients
+      end
+  end
+
+  root 'cocktails#index'
 end
